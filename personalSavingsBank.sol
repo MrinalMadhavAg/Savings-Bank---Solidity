cdc1a1dd439cd87502a7f9f891eb3186d3d7820f
MrinalMadhavAg <madhavagarwal255@gmail.com>
2026-01-03 00:11:53 +0530

    codeupdated

--- personalSavingsBank.sol
+++ personalSavingsBank.sol
@@ -4,6 +4,17 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract SavingsBank
 {
+    address public owner;
+
+    constructor()
+    {
+        owner= msg.sender;
+    }
+    modifier onlyOwner()
+    {
+        require(msg.sender==owner, "Only owner can use this function");
+        _;
+    }
     function deposit() public payable
     {
         require(msg.value > 0, "Put some value to deposit");
@@ -12,12 +23,12 @@ contract SavingsBank
     {
        return address(this).balance; 
     }
-    function withdraw(uint _amount) public
+    function withdraw(uint _amount) public onlyOwner
     {
         require(_amount > 0, "Amount must be greater than 0");
         require(address(this).balance>=_amount, "Insufficient Balance");
 
-        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        (bool success, ) = owner.call{value: _amount}("");
         require(success, "Transfer failed.");
     }
 } 
